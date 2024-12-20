690168ab5dc0ade05993b94f76b39ca49f9fcb1b
triter <vofka99@rambler.ru>
2024-12-20 15:35:36 +0300

    fix typos in config

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -15,7 +15,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.incrseach = true
+vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
@@ -25,4 +25,4 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = 80
+vim.opt.colorcolumn = "80"
